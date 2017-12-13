bdbe18a89fc721583b35ea48c1febd9bdfc17a42
Alberto <amarturelo@gmail.com>
2017-12-13 00:18:36 -0500

    [ADD] SplashPresenter

--- app/src/main/java/com/chaskify/android/ui/fragments/launch/SplashContract.java
+++ app/src/main/java/com/chaskify/android/ui/fragments/launch/SplashContract.java
@@ -1,8 +1,16 @@
 package com.chaskify.android.ui.fragments.launch;
 
+import com.chaskify.android.shared.BaseContract;
+
 /**
  * Created by alberto on 12/12/17.
  */
 
 public class SplashContract {
+    interface View extends BaseContract.View {
+        void showError(Exception e);
+    }
+
+    interface Presenter extends BaseContract.Presenter<SplashContract.View> {
+    }
 }
