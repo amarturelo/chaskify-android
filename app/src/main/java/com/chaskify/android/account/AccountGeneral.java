63b2e6bd8d8ec78abe6214e8dcaebba97bc14e92
Alberto <amarturelo@gmail.com>
2017-12-13 22:33:28 -0500

    [ADD] session to accounts device

---
+++ app/src/main/java/com/chaskify/android/account/AccountGeneral.java
@@ -0,0 +1,19 @@
+package com.chaskify.android.account;
+
+public class AccountGeneral {
+    /**
+     * Account name
+     */
+    public static final String ACCOUNT_NAME = "Chaskify";
+
+    /**
+     * Auth token types
+     */
+    public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
+    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an Chaskify account";
+
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an Chaskify account";
+
+    //public static final ServerAuthenticate sServerAuthenticate = new ParseComServerAuthenticate();
+}
--- app/src/main/java/com/chaskify/android/ui/activities/LaunchActivity.java
+++ app/src/main/java/com/chaskify/android/ui/activities/LaunchActivity.java
@@ -10,6 +10,15 @@ import com.chaskify.android.ui.fragments.launch.LoginFragment;
 
 public class LaunchActivity extends BaseActivity {
 
+    public final static String ARG_ACCOUNT_TYPE = "ACCOUNT_TYPE";
+    public final static String ARG_AUTH_TYPE = "AUTH_TYPE";
+    public final static String ARG_ACCOUNT_NAME = "ACCOUNT_NAME";
+    public final static String ARG_IS_ADDING_NEW_ACCOUNT = "IS_ADDING_ACCOUNT";
+
+    public static final String KEY_ERROR_MESSAGE = "ERR_MSG";
+
+    public final static String PARAM_USER_PASS = "USER_PASS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +28,17 @@ public class LaunchActivity extends BaseActivity {
         initActivity(savedInstanceState);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
     private void initActivity(Bundle savedInstanceState) {
-        if (findFragment(LaunchFragment.class) == null) {
-            loadRootFragment(R.id.fragment, LaunchFragment.newInstance());
-        }
+        if (savedInstanceState == null)
+            if (isAddingNewAccount())
+                loadRootFragment(R.id.fragment, LoginFragment.newInstance());
+            else
+                loadRootFragment(R.id.fragment, LaunchFragment.newInstance());
     }
 
     @Override
@@ -31,4 +47,7 @@ public class LaunchActivity extends BaseActivity {
     }
 
 
+    private boolean isAddingNewAccount() {
+        return getIntent().getBooleanExtra(ARG_IS_ADDING_NEW_ACCOUNT, false);
+    }
 }
